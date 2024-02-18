5a01789755045450bb4ea2a7296b0095093995b2
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-02-18 17:42:00 +0100

    Render Movies to Screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
+// Import Movies Array
 import movies from './movies.js';
+
+// DOM Elements
+const container = document.querySelector('section');
+
+// Render Data
+const renderMovies = (arr) => {
+  const data = arr
+    .map((movie) => {
+      return `
+        <div class="movies">
+            <p class="movies-title">${movie[0]}</p>
+            <p>${movie[1]}</p>
+            <p>${movie[2]}</p>
+            <p>${movie[3]}</p>
+            ${movie[4].map((item) => `<p>${item}</p>`).join('')}
+            <p>${movie[5]}</p>
+        </div>
+    `;
+    })
+    .join('');
+
+  container.innerHTML = data;
+};
+
+// renderMovies(movies);
